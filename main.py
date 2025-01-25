5a3eb7ffd60a3fdb8484edb69400608623462067
ben@blueinno <ben@blueinnotechnology.com>
2025-01-25 12:18:41 +0800

    lesson 3 part 3

--- main.py
+++ main.py
@@ -5,39 +5,13 @@ Q1:
 
 # TODO: Write your code here
 
-##################################################
-'''
-Q2:
-'''
+current_balance = 1000
+interest_rate = 0.065
+new_deposit = 100
 
-# TODO: Write your code here
-
-##################################################
-'''
-Q3:
-'''
-
-# TODO: Write your code here
-
-##################################################
-'''
-Q4:
-'''
-
-# TODO: Write your code here
-
-##################################################
-'''
-Q5:
-'''
-
-# TODO: Write your code here
-
-##################################################
-'''
-Q6:
-'''
-
-# TODO: Write your code here
-
-##################################################
+for i in range(1, 26):
+    interest = current_balance * interest_rate
+    new_balance = current_balance + interest
+    print(f'{i:02d}: current balance: {current_balance:.2f}, interest: {
+          interest:.2f}, deposit: {new_deposit}, new balance: {new_balance:.2f}')
+    current_balance = new_balance + new_deposit
